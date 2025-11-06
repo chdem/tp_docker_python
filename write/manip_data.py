0f9dd3451e0351a92a6d820ab95e167fc5f0dc9e
ch <outframify@gmail.com>
2025-11-06 13:28:03 +0100

    feat ajout ihm + bugfix supression

--- write/manip_data.py
+++ write/manip_data.py
@@ -2,11 +2,6 @@ from exceptions_custom import InvalidTitleException, InvalidYearException,Invali
 from models.Movie import Movie
 import csv
 
-def show_movies_menu():
-    rows = __read_rows()
-    for row in rows:
-        print(row)
-
 def add_movie(titre, annee_production, genre, age_limite):
     movie = Movie(titre, annee_production, genre, age_limite)
     movie_file = open("write/data/movies.csv", "a")
@@ -19,7 +14,9 @@ def update_movie(id: int, titre, annee_production, genre, age_limite):
 
 def remove_movie(id: int):
     rows = __read_rows()
-    rows.pop(id)
+    for i, row in row:
+        if row[0] == id - 1:
+            rows.pop(i)
     __write_rows(rows)
 
 def __read_rows():
@@ -35,14 +32,51 @@ def __write_rows(rows: list):
 def __format_csv(movie: Movie, id=None):
     return f"{id or movie._id},{movie._titre},{movie._annee_production},{movie._age_limite}\n"
 
+def main_menu():
+    while True:
+        show_choices()
+        choice = input("Choisir votre action: ")
+        match choice:
+            case "1": show_movies_menu()
+            case "2": add_movie_menu()
+            case "3": modify_movie_menu()
+            case "4": remove_movie_menu()
+            case "5": exit()
 
-def modify_movie_menu(id: int):
-    title = is_valid(input_title)
-    year = is_valid(input_production_year)
-    genre = is_valid(input_production_genre)
-    age_limite = is_valid(input_age)
-    update_movie(id, title, year, genre, age_limite)
 
+def show_choices():
+    print("1. Afficher les films")
+    print("2. Ajouter un film")
+    print("3. Modifier un film")
+    print("4. Supprimer un film")
+    print("4. Exit")
+
+def show_movies_menu():
+    rows = __read_rows()
+    for row in rows:
+        print(row)
+
+def modify_movie_menu():
+    while True: 
+        choice = input("Numéro du film à modifier : ")
+        if not choice.isdigit():
+            print("Entrez un numéro de film valide !")
+            pass
+        title = is_valid(input_title)
+        year = is_valid(input_production_year)
+        genre = is_valid(input_production_genre)
+        age_limite = is_valid(input_age)
+        update_movie(int(choice), title, year, genre, age_limite)
+        break
+
+def remove_movie_menu():
+    while True: 
+        choice = input("Numéro du film à modifier : ")
+        if not choice.isdigit():
+            print("Entrez un numéro de fil valide !")
+            pass
+        remove_movie(int(choice))
+        break
 
 def add_movie_menu():
     title = is_valid(input_title)
@@ -88,5 +122,3 @@ def input_age():
     if not (0 < age <= 18):
         raise InvalidAgeLimitException.InvalidAgeLimitException()
     return age
-
-show_movies_menu()
